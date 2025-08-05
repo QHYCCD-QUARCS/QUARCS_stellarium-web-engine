3b4d856438b2632820eb7a6c96a23f72269e2041
CJQ <QUARCS@qhyccd.com>
2025-08-05 17:08:56 +0800

    上传完整项目，包含之前被排除的文件

---
+++ build/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/Logger.cpp" "CMakeFiles/client.dir/Logger.cpp.o" "gcc" "CMakeFiles/client.dir/Logger.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_stellarium-web-engine/build/client_autogen/mocs_compilation.cpp" "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/main.cpp" "CMakeFiles/client.dir/main.cpp.o" "gcc" "CMakeFiles/client.dir/main.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/mainwindow.cpp" "CMakeFiles/client.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/client.dir/mainwindow.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/myclient.cpp" "CMakeFiles/client.dir/myclient.cpp.o" "gcc" "CMakeFiles/client.dir/myclient.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/platesolveworker.cpp" "CMakeFiles/client.dir/platesolveworker.cpp.o" "gcc" "CMakeFiles/client.dir/platesolveworker.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/tools.cpp" "CMakeFiles/client.dir/tools.cpp.o" "gcc" "CMakeFiles/client.dir/tools.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/websocketclient.cpp" "CMakeFiles/client.dir/websocketclient.cpp.o" "gcc" "CMakeFiles/client.dir/websocketclient.cpp.o.d"
+  "/home/quarcs/workspace/QUARCS/QUARCS_QT-SeverProgram/src/websocketthread.cpp" "CMakeFiles/client.dir/websocketthread.cpp.o" "gcc" "CMakeFiles/client.dir/websocketthread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
